210a891e291bb12ccb075c6822019532b1d8fa3a
unknown <wangying@xdc.xinyoudc.com>
2015-12-22 11:03:38 +0800

    remove all

--- Assets/Editor/LightMapAsset.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LightMapAsset : ScriptableObject
-{
-    public Texture2D[] lightmapFar;
-    public Texture2D[] lightmapNear;
-}
--- Assets/Scripts/Common/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Constants{
-    public const int LIMIT_MEMORY_SIZE = 1024;
-    public const int PROCESSOR_COUNT= 2;
-    public static bool IS_FIRST_INIT_GAME = true;
-}
